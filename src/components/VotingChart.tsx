09e7ddbee6a241faf11d6f9a298c07bb51969024
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-03-21 11:15:48 +0300

    one decimal after the dot

--- src/components/VotingChart.tsx
+++ src/components/VotingChart.tsx
@@ -55,19 +55,19 @@ const VotingChart = ({yesCount, noCount, nwvCount, abstainCount}) => {
 
     return (
         <Grid item container direction="row" justifyContent="flex-start" sx={{position: "absolute"}}>
-            <Tooltip title={"Yes: " + item.yes + "%"}>
+            <Tooltip title={"Yes: " + item.yes.toFixed(1) + "%"}>
                 {/*@ts-ignore*/}
                 <Grid item sx={{height: "30px", width: item.yes / 2 + "%", backgroundColor: context.colors.yes, float: "left"}}/>
             </Tooltip>
-            <Tooltip title={"No: " + item.no + "%"}>
+            <Tooltip title={"No: " + item.no.toFixed(1) + "%"}>
                 {/*@ts-ignore*/}
                 <Grid item sx={{height: "30px", width: item.no / 2 + "%", backgroundColor: context.colors.no, float: "left"}}/>
             </Tooltip>
-            <Tooltip title={"No with veto: " + item.nwv + "%"}>
+            <Tooltip title={"No with veto: " + item.nwv.toFixed(1) + "%"}>
                 {/*@ts-ignore*/}
                 <Grid item sx={{height: "30px", width: item.nwv / 2 + "%", backgroundColor: context.colors.nwv, float: "left"}}/>
             </Tooltip>
-            <Tooltip title={"Abstain: " + item.abstain + "%"}>
+            <Tooltip title={"Abstain: " + item.abstain.toFixed(1) + "%"}>
                 {/*@ts-ignore*/}
                 <Grid item sx={{height: "30px", width: item.abstain / 2 + "%", backgroundColor: context.colors.abstain, float: "left"}}/>
             </Tooltip>
